5c9d5a26a94c46b10d1af15fca03d753d6c0b7ca
qamshly1994 <abdulrazaqalali2@gmail.com>
2026-02-07 02:25:21 +0300

    Configure server to run on specified port
    Added server port configuration for deployment.

--- app.py
+++ app.py
@@ -137,5 +137,8 @@ with app.app_context():
         db.session.add(admin)
         db.session.commit()
 
+# ===== Run server =====
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
